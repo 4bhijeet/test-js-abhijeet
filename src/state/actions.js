2482c3c0078c94f4c4082838065d377cf6932f48
4bhijeet <abhijeet.deshpande.89@gmail.com>
2017-04-06 12:01:44 -0400

    Completed the basic use case requirements

--- src/state/actions.js
+++ src/state/actions.js
@@ -14,7 +14,24 @@ export function getPopularMovies () {
   //    this is used in src/components/movies-list.js line 25
   //
 
-  const combinedResults = []
+
+  //Using the thenby library for sorting based on multiple criterias
+  var firstBy = require('thenby');
+ 
+  //combining the movies array into single array object for easy iteration.
+  var moviesCombined = [];
+  for(var i=0; i<movies.length; i++){
+    moviesCombined = moviesCombined.concat(movies[i]);
+  }
+
+  //Here we create an array that contains only fields we want to display on the front end
+  var moviesWithReleaseYear = getMoviesWithReleaseYear(moviesCombined);
+  
+  //Sorting the movies array based on release date and then title.
+  const combinedResults = moviesWithReleaseYear.sort(
+      firstBy(function (v1, v2) { return v1.releaseYear - v2.releaseYear; })
+      .thenBy('title')
+  );
 
   return {
     type: 'GET_MOVIES_SUCCESS',
@@ -23,3 +40,25 @@ export function getPopularMovies () {
 }
 
 
+//This function creates an array of movies with rating between 4 and 5 and also adds another field releaseYear for sorting
+function getMoviesWithReleaseYear(movies){
+  var moviesWithReleaseYear = [];
+  for (var i=0; i<movies.length; i++){
+    var movie = movies[i];
+    var releaseYear = moment(movie.releaseDate).year();  //movie.title.substring(movie.title.lastIndexOf("(")+1, movie.title.lastIndexOf(")"))
+    var title = movie.title;
+    if(movie.rating >= 4 && movie.rating <= 5){
+      moviesWithReleaseYear.push({
+        'title' : title, 
+        'releaseYear' : releaseYear,  
+        'image' : movie.image, 
+        'price' : movie.price, 
+        'rating' : movie.rating,
+        'releaseDate' : movie.releaseDate,
+        'genres' : movie.genres
+      });
+    }
+    
+  }
+  return moviesWithReleaseYear;
+}
